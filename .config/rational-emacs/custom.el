c180094f5df7fe5bb2b36f41e12541ebd1299a40
wereket <dagnachewa@gmail.com>
2022-07-15 06:52:06 -0400

    rational-emacs

---
+++ .config/rational-emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck-clj-kondo clojure-mode dired-icon all-the-icons-completion all-the-icons-dired dashboard modus-themes doom-themes keycast org-appear helpful elisp-demos doom-modeline all-the-icons vertico orderless marginalia embark-consult embark corfu-doc consult cape use-package))
+ '(rational-ui-default-font '(:font "JetBrains Mono" :weight light :height 185)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minibuffer-prompt ((t (:foreground "dark gray" :inherit modus-themes-prompt)))))
